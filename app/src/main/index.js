3f0de272406bed4e8c2d8126d000b0f57e711109
Leon-Frenzl <leonfrenzl@googlemail.com>
2023-06-09 23:27:42 +0200

    New Setup

--- app/src/main/index.js
+++ app/src/main/index.js
@@ -1,56 +1,83 @@
-import { app, shell, BrowserWindow, session } from 'electron'
-import { join } from 'path'
-import { electronApp, optimizer, is } from '@electron-toolkit/utils'
-import icon from '../../resources/icon.png?asset'
-import apiServer from './server'
+import { app, shell, BrowserWindow, ipcMain } from 'electron';
+import { join } from 'path';
+import { electronApp, optimizer, is } from '@electron-toolkit/utils';
+import icon from '../../resources/icon.png?asset';
+import { parseRssFeed } from './rssParser';
+
+// Dummy data for RSS feeds
+let rssFeeds = [
+  { id: 1, url: 'https://example.com/rss/feed1' },
+  { id: 2, url: 'https://example.com/rss/feed2' },
+];
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 900,
     height: 670,
     show: false,
-    autoHideMenuBar: true,
+    autoHideMenuBar: false,
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
-      sandbox: false
-    }
-  })
+      sandbox: false,
+    },
+  });
 
   mainWindow.on('ready-to-show', () => {
-    mainWindow.show()
-  })
+    mainWindow.show();
+  });
 
   mainWindow.webContents.setWindowOpenHandler((details) => {
-    shell.openExternal(details.url)
-    return { action: 'deny' }
-  })
+    shell.openExternal(details.url);
+    return { action: 'deny' };
+  });
 
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
+    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL']);
   } else {
-    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
+    mainWindow.loadFile(join(__dirname, '../renderer/index.html'));
   }
 }
 
 app.whenReady().then(() => {
-  electronApp.setAppUserModelId('com.electron')
+  electronApp.setAppUserModelId('com.electron');
   app.on('browser-window-created', (_, window) => {
-    optimizer.watchWindowShortcuts(window)
-  })
-  
-  createWindow()
+    optimizer.watchWindowShortcuts(window);
+  });
+
+  createWindow();
 
   app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
-
-
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    apiServer.close()
-    app.quit()
+    app.quit();
   }
-})
+});
+
+// CRUD endpoints for RSS feeds
+ipcMain.on('request-rss-feeds', (event) => {
+  event.reply('response-rss-feeds', rssFeeds);
+});
+
+ipcMain.on('add-rss-feed', (event, rssFeedUrl) => {
+  const newRssFeed = { id: rssFeeds.length + 1, url: rssFeedUrl };
+  rssFeeds.push(newRssFeed);
+  event.reply('rss-feed-added', newRssFeed);
+});
+
+ipcMain.on('delete-rss-feed', (event, rssFeedId) => {
+  rssFeeds = rssFeeds.filter((feed) => feed.id !== rssFeedId);
+  event.reply('rss-feed-deleted', rssFeedId);
+});
+
+ipcMain.on('update-rss-feed', (event, updatedRssFeed) => {
+  const index = rssFeeds.findIndex((feed) => feed.id === updatedRssFeed.id);
+  if (index !== -1) {
+    rssFeeds[index] = updatedRssFeed;
+    event.reply('rss-feed-updated', updatedRssFeed);
+  }
+});
--- app/src/renderer/src/App.jsx
+++ app/src/renderer/src/App.jsx
@@ -1,7 +1,35 @@
+import React, { useState, useEffect } from 'react';
+
 function App() {
+  const [response, setResponse] = useState([]);
+
+  const requestRssFeeds = () => {
+    window.electron.ipcRenderer.send('request-rss-feeds');
+  };
+
+  useEffect(() => {
+    const ipcRenderer = window.electron.ipcRenderer;
+
+    ipcRenderer.on('response-rss-feeds', (event, rssFeeds) => {
+      setResponse(rssFeeds);
+    });
+
+    // Cleanup: remove the listener when the component unmounts
+    return () => {
+      ipcRenderer.removeAllListeners('response-rss-feeds');
+    };
+  }, []);
+
   return (
-    <h1>Hello World</h1>
-  )
+    <div>
+      <button onClick={requestRssFeeds}>Request Rss Feeds</button>
+      <div>
+        {response.map((feed) => (
+          <div key={feed.id}>{feed.url}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
